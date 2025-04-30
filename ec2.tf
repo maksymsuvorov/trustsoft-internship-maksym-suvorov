6c12ef3d67fb08ad97e0b8178c1be9c21566a020
Maksym Suvorov <suvormak@fit.cvut.cz>
2025-04-30 09:35:31 +0200

    Add userdata.sh content

--- ec2.tf
+++ ec2.tf
@@ -5,23 +5,8 @@ resource "aws_instance" "ec2-1" {
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   iam_instance_profile = aws_iam_instance_profile.ssm-instance-profile.name
 
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    yum update -y
-
-    yum install -y amazon-ssm-agent
-    systemctl enable amazon-ssm-agent
-    systemctl start amazon-ssm-agent
-    
-    yum install -y httpd
-    
-    echo "<h1>Hello from EC2 instance $(hostname)</h1>" > /var/www/html/index.html
-
-    systemctl enable httpd
-    systemctl start httpd
-  EOF
-  )
-
+  user_data = file("scripts/userdata.sh")
+  
   tags = {
     Name: "ec2-1-internship-maksym"
   }
@@ -34,22 +19,7 @@ resource "aws_instance" "ec2-2" {
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   iam_instance_profile = aws_iam_instance_profile.ssm-instance-profile.name
 
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    yum update -y
-    
-    yum install -y amazon-ssm-agent
-    systemctl enable amazon-ssm-agent
-    systemctl start amazon-ssm-agent
-
-    yum install -y httpd
-
-    echo "<h1>Hello from EC2 instance $(hostname)</h1>" > /var/www/html/index.html
-
-    systemctl enable httpd
-    systemctl start httpd
-  EOF
-  )
+  user_data = file("scripts/userdata.sh")
 
   tags = {
     Name: "ec2-2-internship-maksym"
---
+++ scripts/userdata.sh
@@ -0,0 +1,13 @@
+    #!/bin/bash
+    yum update -y
+    
+    yum install -y amazon-ssm-agent
+    systemctl enable amazon-ssm-agent
+    systemctl start amazon-ssm-agent
+
+    yum install -y httpd
+
+    echo "<h1>Hello from EC2 instance $(hostname)</h1>" > /var/www/html/index.html
+
+    systemctl enable httpd
+    systemctl start httpd
